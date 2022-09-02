7a7788cb5c9fdccaa0172be04600154cd5e5dceb
isack <isack@LAPTOP-KBT7Q9Q0>
2022-09-01 19:08:45 -0700

    Agregar archivos de proyecto.

---
+++ Practica1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nombre: Isack Hernandez Valenzuela");
+            Console.WriteLine("Ciudad: Mexicali,B.C");
+            Console.WriteLine("Fecha de nacimiento: 27 abril 1998");
+            Console.WriteLine("Edad: 24 Años");
+            Console.ReadKey();
+        }
+    }
+}
